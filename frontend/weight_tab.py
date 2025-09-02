87d787f029aa37c2ca5bd09cd5569523041c94ee
Nikos Iliakis <nickiliak847@gmail.com>
2025-09-03 00:37:47 +0200

    Fixed  UI for Weight gain display

--- frontend/weight_tab.py
+++ frontend/weight_tab.py
@@ -2,7 +2,7 @@ import streamlit as st
 from datetime import datetime
 import requests
 import pandas as pd
-
+import plotly.express as px
 
 API_URL = "http://localhost:8000"
 
@@ -10,10 +10,10 @@ API_URL = "http://localhost:8000"
 def weight_tab():
     col1, col2 = st.columns(2)
     with col1:
-        start_date = st.date_input("Start Date", datetime(2025, 9, 2))
+        start_date = st.date_input("Start Date", datetime(2025, 9, 1))
 
     with col2:
-        end_date = st.date_input("End Date", datetime(2025, 9, 3))
+        end_date = st.date_input("End Date", datetime(2025, 9, 30))
     
     if st.button("Get Weight Analytics"):
         payload = {
@@ -23,13 +23,26 @@ def weight_tab():
 
         response = requests.get(f"{API_URL}/weights/", params=payload)
         response = response.json()
-    
-        print(response)
-        
-        df = pd.DataFrame(response)
-        df = df.sort_values("logged_at")
-        st.write("Logged weights")
-        st.dataframe(df)
 
-        # # Plot line chart
-        st.line_chart(df.set_index("logged_at")["weight_kg"])
+        df = pd.DataFrame(response)
+        df.rename(columns={"weight_kg": "weight", "logged_at": "date"}, inplace=True)
+        df = df.dropna(subset=["weight"]) 
+        df = df.sort_values("date")
+        
+        start_date = df["date"].min()
+        final_date = df["date"].max()
+        
+        least_weight = df["weight"].min()
+        max_weight = df["weight"].max()
+        
+        start_weight = df.loc[df["date"] == start_date, "weight"].iloc[0]
+        final_weight = df.loc[df["date"] == final_date, "weight"].iloc[0]
+        weight_diff = final_weight - start_weight
+        
+        fig = px.line(df, x="date", y="weight", title=f"Weight gain over time,  {weight_diff:.2f} kg", markers=True)
+        
+        fig.update_yaxes(range=[least_weight, max_weight], fixedrange=True)
+        fig.update_xaxes(range=[start_date, final_date], fixedrange=True)
+        
+        fig.update_traces(hovertemplate='Date: %{x}<br>Weight: %{y} kg')
+        st.plotly_chart(fig)
