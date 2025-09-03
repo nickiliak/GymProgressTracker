cddd7dc2b82b683e5a988a3f34dbb62fc35e46ce
Nikos Iliakis <nickiliak847@gmail.com>
2025-09-03 17:32:14 +0200

    Attempt fixing loggig not found

--- backend/db_helper.py
+++ backend/db_helper.py
@@ -2,7 +2,7 @@ import datetime
 import mysql.connector
 import random
 from contextlib import contextmanager
-from logging_setup import setup_logger
+from backend.logging_setup import setup_logger
 
 logger = setup_logger('db_helper')
 
